dcf9c8d4a937bdd79b64dbc1e812cf276688e56e
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-01-13 22:16:08 +0500

    update: app structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
 import "./index.scss";
-const App = () => {
-  return (
-    <>
-      <h1>Hi!</h1>
-      <h2>Test FIX</h2>
-    </>
-  );
-};
 
-export default App;
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
